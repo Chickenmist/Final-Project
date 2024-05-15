134dbd559f501106f8d060f149d1726031f56b3b
Chickenmist <wpimlatt@gmail.com>
2024-05-15 14:41:07 -0400

    Added Sprite sheets and started on getting the player class working

--- Final Project/Game1.cs
+++ Final Project/Game1.cs
@@ -1,13 +1,25 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace Final_Project
 {
     public class Game1 : Game
     {
+        Player player;
+
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        KeyboardState keyboardState;
+
+        Texture2D playerAttackTexture;
+        Texture2D playerIdleTexture; //53x58
+        Texture2D playerDeathTexture;
+        Texture2D playerRunTexture;
+        Texture2D playerRunAttackTexture;
+        Texture2D playerWalkTexture; 
 
         public Game1()
         {
@@ -21,6 +33,8 @@ namespace Final_Project
             // TODO: Add your initialization logic here
 
             base.Initialize();
+            player = new Player(playerIdleTexture, 50, 50);
+            
         }
 
         protected override void LoadContent()
@@ -28,14 +42,46 @@ namespace Final_Project
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            
+            playerAttackTexture = Content.Load<Texture2D>("Skeleton Attack");
+            playerIdleTexture = Content.Load<Texture2D>("Skeleton Idle");
+            playerDeathTexture = Content.Load<Texture2D>("Skeleton Dead");
+            playerRunTexture = Content.Load<Texture2D>("Skeleton Run");
+            playerRunAttackTexture = Content.Load<Texture2D>("Skeleton Run Attack");
+            playerWalkTexture = Content.Load<Texture2D>("Skeleton Walk");
+
         }
 
         protected override void Update(GameTime gameTime)
         {
+            keyboardState = Keyboard.GetState();
+
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+
+            if (keyboardState.IsKeyDown(Keys.A) && keyboardState.IsKeyDown(Keys.LeftShift))
+            {
+
+            }
+            else if (keyboardState.IsKeyDown(Keys.D) && keyboardState.IsKeyDown(Keys.LeftShift))
+            {
+
+            }
+            else if (keyboardState.IsKeyDown(Keys.A))
+            {
+
+            }
+            else if (keyboardState.IsKeyDown(Keys.D))
+            {
+
+            }
+
+            if (keyboardState.IsKeyDown(Keys.Space))
+            {
+
+            }
 
             base.Update(gameTime);
         }
@@ -45,6 +91,13 @@ namespace Final_Project
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            player.Draw(_spriteBatch);
+
+            _spriteBatch.End();
+
 
             base.Draw(gameTime);
         }
---
+++ Final Project/Player.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project
+{
+    internal class Player
+    {
+
+        private Texture2D _texture;
+        private Rectangle _location;
+        private Vector2 _speed;
+        private float _seconds;
+
+        private Rectangle _spriteFrame;
+
+
+        public Player(Texture2D texture, int x, int y)
+        {
+            _texture = texture;
+            _location = new Rectangle(x, y, 53, 58);
+            _speed = new Vector2();
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            _seconds += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            
+
+        }
+
+        public void Draw(SpriteBatch spriteBatch) 
+        {
+            //spriteBatch.Draw(_textureList[PlayerState.Dead], _location, _spriteFrame, Color.White);
+        }
+    }
+}
